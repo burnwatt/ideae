5e5fe0089c022e3c074d349598647a2c5dfdf02e
Josh Burnwatt <burnwattj@gmail.com>
2020-11-15 19:17:43 -0600

    add fivethirtyeight styling

--- python_basics/data_viz.py
+++ python_basics/data_viz.py
@@ -1,7 +1,19 @@
 import matplotlib.pyplot as plt
 
+inputs = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+
+# initialize our chart
+plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(inputs, squares, linewidth=3)
+
+# Set title and axis labels
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=13)
+ax.set_ylabel("Square of Value", fontsize=13)
+
+# Set tick labels
+ax.tick_params(axis='both', labelsize=13)
 
 plt.show()
