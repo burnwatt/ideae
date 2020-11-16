034343b87956efe7bb43cf1b4221a766e3e12737
Josh Burnwatt <burnwattj@gmail.com>
2020-11-16 15:12:03 -0600

    covert to simulate walking path

--- python_basics/rw_viz.py
+++ python_basics/rw_viz.py
@@ -3,18 +3,18 @@ from random_walk import RandomWalk
 
 # Generate random walks
 while True:
-    rw = RandomWalk(50_000)
+    rw = RandomWalk()
     rw.fill_walk()
 
     # Plot walks
     plt.style.use('seaborn-dark-palette')
     fig, ax = plt.subplots(figsize=(12,6), dpi=128)
     point_nums = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_nums, cmap=plt.cm.Blues, edgecolors='none', s=1)
+    ax.plot(rw.x_values, rw.y_values, linewidth=1)
 
     # emphasis on the starting and ending points
-    ax.scatter(0,0,c='green', edgecolors='none', s=13)
-    ax.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=13)
+    ax.scatter(0,0,c='green', edgecolors='none', s=42)
+    ax.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=42)
 
     # Remove axes
     ax.get_xaxis().set_visible(False)
