b11adf34e235b72f2dc27fcb67231ba1dd735379
Josh Burnwatt <burnwattj@gmail.com>
2020-11-16 12:46:20 -0600

    Add user input for multiple walks

--- python_basics/rw_viz.py
+++ python_basics/rw_viz.py
@@ -2,11 +2,16 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 # Generate random walks
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Plot walks
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=13)
-plt.show()
+    # Plot walks
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=13)
+    plt.show()
+
+    keep_walking = input("Take another walk? (y/n): ")
+    if keep_walking == 'n':
+        break
