0aff5d04dec8d79dc5643d6029a032ac5c2fab67
Josh Burnwatt <burnwattj@gmail.com>
2020-11-16 15:01:16 -0600

    style and get_step refactor

--- python_basics/random_walk.py
+++ python_basics/random_walk.py
@@ -11,20 +11,20 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+        """Calulate x,y positions for next step"""
+        direction = choice([1, -1])
+        distance = choice([0,1,2,3,4])
+        return direction * distance
+
     def fill_walk(self):
         """Calculate all points in a walk"""
-
         # Keep generating steps until the walk has reached the desired length
         while len(self.x_values) < self.num_points:
 
             # decide which direction to go and how far
-            x_direction = choice([1, -1])
-            x_distance = choice([0,1,2,3,4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0,1,2,3,4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # reject moves that go nowhere
             if x_step == 0 and y_step == 0:
--- python_basics/rw_viz.py
+++ python_basics/rw_viz.py
@@ -3,13 +3,23 @@ from random_walk import RandomWalk
 
 # Generate random walks
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot walks
-    plt.style.use('classic')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=13)
+    plt.style.use('seaborn-dark-palette')
+    fig, ax = plt.subplots(figsize=(12,6), dpi=128)
+    point_nums = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_nums, cmap=plt.cm.Blues, edgecolors='none', s=1)
+
+    # emphasis on the starting and ending points
+    ax.scatter(0,0,c='green', edgecolors='none', s=13)
+    ax.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=13)
+
+    # Remove axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_walking = input("Take another walk? (y/n): ")
